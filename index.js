8fe0c16d88dd68736be02964eab2f9c2a6df8318
DuckBat <32737124+duckbat@users.noreply.github.com>
2024-09-07 16:32:23 +0300

    username function

--- index.js
+++ index.js
@@ -9,25 +9,37 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  console.log(`User with id:${socket.id} has joined`);
-  console.log(`Current connections: ${io.engine.clientsCount}`);
-  console.log("----------------")
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-    console.log(socket.id);
-    console.log(`Current connections: ${io.engine.clientsCount}`);
-    console.log("----------------")
-  });
-});
+const users = {}; // Object to keep track of usernames by socket ID
 
 io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+  console.log(`Current connections: ${io.engine.clientsCount}`);
+  console.log("----------------");
+
+  // Handle setting the username
+  socket.on("set username", (username) => {
+    users[socket.id] = username; // Store the username with the socket ID
+    console.log(`User with id ${socket.id} set username to ${username}`);
+    io.emit("chat message", `${username} has joined the chat`);
+  });
+
+  // Handle chat messages
   socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+    const username = users[socket.id] || "Anonymous"; // Fallback if no username is set
+    io.emit("chat message", `${username}: ${msg}`);
+  });
+
+  // Handle user disconnection
+  socket.on("disconnect", () => {
+    const username = users[socket.id] || `User with id ${socket.id}`;
+    console.log(`User disconnected: ${socket.id}`);
+    console.log(`Current connections: ${io.engine.clientsCount}`);
+    console.log("----------------");
+    io.emit("chat message", `${username} has left the chat`);
+    delete users[socket.id]; // Remove the user from the users object
   });
 });
 
 server.listen(3000, () => {
-  console.log("listening on http://localhost:3000");
+  console.log("Listening on http://localhost:3000");
 });
