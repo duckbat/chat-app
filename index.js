3bd79a48d94372b98aa3b8abe1d7cb1a5d2d8a6d
DuckBat <32737124+duckbat@users.noreply.github.com>
2024-09-08 01:35:21 +0300

    added heroku functionality

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Listening on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
