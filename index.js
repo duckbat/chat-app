bd33217de4885cadfc9edb346afd69c1ec538373
DuckBat <32737124+duckbat@users.noreply.github.com>
2024-09-07 22:25:03 +0300

    new version came

--- index.js
+++ index.js
@@ -4,39 +4,56 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const path = require("path");
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
+app.use(express.static(path.join(__dirname, "public"))); // Serve static files from the 'public' folder
 
-const users = {}; // Object to keep track of usernames by socket ID
+const users = {}; // Object to keep track of usernames by id
+const msg = {
+  general: [],
+  jokes: [],
+  coding: [],
+};
 
 io.on("connection", (socket) => {
   console.log(`User connected: ${socket.id}`);
-  console.log(`Current connections: ${io.engine.clientsCount}`);
-  console.log("----------------");
 
   // Handle setting the username
   socket.on("set username", (username) => {
-    users[socket.id] = username; // Store the username with the socket ID
+    users[socket.id] = username;
     console.log(`User with id ${socket.id} set username to ${username}`);
-    io.emit("chat message", `${username} has joined the chat`);
   });
 
   // Handle chat messages
-  socket.on("chat message", (msg) => {
-    const username = users[socket.id] || "Anonymous"; // Fallback if no username is set
-    io.emit("chat message", `${username}: ${msg}`);
+  socket.on("send message", ({ content, to, sender, chatName, isChannel }) => {
+    const payload = {
+      content,
+      chatName,
+      sender,
+    };
+
+    if (isChannel) {
+      socket.to(to).emit("new message", payload); // Send message to room
+      if (msg[chatName]) {
+        msg[chatName].push(payload); // Save the message in the channel's history
+      }
+    } else {
+      // Handle direct messages here if needed
+      socket.to(to).emit("new message", payload);
+    }
+  });
+
+  // Handle user joining a room
+  socket.on("join room", (roomName, cb) => {
+    socket.join(roomName);
+    cb(msg[roomName] || []); // Send existing messages of the room to the client
+    console.log(`${users[socket.id]} joined room: ${roomName}`);
   });
 
   // Handle user disconnection
   socket.on("disconnect", () => {
-    const username = users[socket.id] || `User with id ${socket.id}`;
+    delete users[socket.id];
     console.log(`User disconnected: ${socket.id}`);
-    console.log(`Current connections: ${io.engine.clientsCount}`);
-    console.log("----------------");
-    io.emit("chat message", `${username} has left the chat`);
-    delete users[socket.id]; // Remove the user from the users object
   });
 });
 
